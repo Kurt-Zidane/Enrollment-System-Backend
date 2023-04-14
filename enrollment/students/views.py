a08c224256d8a73976e74b60e25abf0e7549acd9
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-04-14 16:49:32 +0800

    added delete feature

--- enrollment/students/views.py
+++ enrollment/students/views.py
@@ -1,11 +1,21 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import StudentSerializer
 from .models import Student
-from rest_framework import generics
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
     serializer_class = StudentSerializer
     queryset = Student.objects.all().order_by('date_joined')
 
+
+def delete(self, request, pk):
+    try:
+        student = self.queryset.get(pk=pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
