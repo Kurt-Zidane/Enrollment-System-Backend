09da75bbbfdc05253a410d948601b5c7ed75b0d0
toledo <kurtzidanetoledo@gmail.com>
2023-04-21 13:16:28 +0800

    Edited urls and views for schedule

--- enrollment/schedules/urls.py
+++ enrollment/schedules/urls.py
@@ -3,11 +3,12 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'schedules', views.ScheduleViewSet)
+router.register(r'schedules', views.SchedulesViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('schedules/<int:pk>/', views.ScheduleViewSet.as_view()),
+    path('schedules/', views.SchedulesViewSet.as_view()),
 
 ]
--- enrollment/schedules/views.py
+++ enrollment/schedules/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import viewsets, generics
 from .serializers import ScheduleSerializer, SubjectSerializer, ProfessorSerializer, StudentSerializer
 from .models import Schedule
@@ -6,13 +8,20 @@ from subjects.models import Subject
 from professor.models import Professor
 from students.models import Student
 
-
-class ScheduleViewSet(viewsets.ModelViewSet):
+class ScheduleViewSet(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = [IsAuthenticated]
     serializer_class = ScheduleSerializer
     queryset = Schedule.objects.all().order_by('date_added')
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+class SchedulesViewSet(generics.ListCreateAPIView):
+    # permission_classes = [IsAuthenticated]
+    serializer_class = ScheduleSerializer
+    queryset = Schedule.objects.all().order_by('date_added')
 
 class SubjectViewSet(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
